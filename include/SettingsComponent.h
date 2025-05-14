c3f109d1d49f76f231279a1df9cb97f9706c659b
Kevin <kevin.monteiro@etu.uca.fr>
2025-05-14 16:22:32 +0200

    Update settings

--- include/SettingsComponent.h
+++ include/SettingsComponent.h
@@ -1,32 +1,16 @@
 #pragma once
+
 #include <juce_audio_devices/juce_audio_devices.h>
-
 #include <juce_audio_utils/juce_audio_utils.h>
-
-
-
 #include <juce_gui_basics/juce_gui_basics.h>
 
 class SettingsComponent : public juce::Component {
 public:
-    explicit SettingsComponent(juce::AudioDeviceManager& deviceManager)
-        : deviceSelector(deviceManager, 0, 2, 0, 2, true, true, false, false)
-    {
-        addAndMakeVisible(label);
-        label.setText("Settings", juce::dontSendNotification);
+    explicit SettingsComponent(juce::AudioDeviceManager& deviceManager);
 
-        addAndMakeVisible(deviceSelector);
-    }
-
-    void resized() override
-    {
-        label.setBounds(10, 10, getWidth() - 20, 30);
-        deviceSelector.setBounds(10, 50, getWidth() - 20, getHeight() - 60);
-    }
+    void resized() override;
 
 private:
     juce::Label label;
     juce::AudioDeviceSelectorComponent deviceSelector;
 };
-
-
