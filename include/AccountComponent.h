0da70f9c6f6627a0c88fef4a16bfd52f2203bc5a
Kevin <kevin.monteiro@etu.uca.fr>
2025-06-02 08:30:33 +0200

    Add chorus effect

--- include/AccountComponent.h
+++ include/AccountComponent.h
@@ -2,15 +2,28 @@
 
 #include <JuceHeader.h>
 
-class AccountComponent : public Component
+class AccountComponent : public juce::Component
 {
 public:
     AccountComponent();
-    void paint(Graphics&) override;
+    void paint(juce::Graphics&) override;
     void resized() override;
 
 private:
-    void apiResponseReceived(const String& content);
-    Label responseLabel;
+    void apiResponseReceived(const juce::String& content);
+    void saveSettings();
+    void importSettings();
+    void changePassword();
+
+    juce::Label titleLabel;
+    juce::Label responseLabel;
+
+    juce::Label emailLabel, usernameLabel;
+    juce::Label emailValueLabel, usernameValueLabel;
+
+    juce::TextButton saveButton, importButton, changePasswordButton;
+
+    juce::Grid grid;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AccountComponent)
 };
